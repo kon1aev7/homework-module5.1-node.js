a2b0fd2c6666cb66239ce6492904bc1139605c94
kon1aev7 <konyaevd1995@gmail.com>
2025-04-18 15:29:21 +0300

    last fx

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -3,16 +3,23 @@ import { Router } from 'express';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../utils/validateBody.js';
 
-import { authRegisterSchema } from '../validation/auth.js';
+import { authRegisterSchema, authLoginSchema } from '../validation/auth.js';
 
-import { registerController } from '../controllers/auth.js';
+import { registerController, loginController } from '../controllers/auth.js';
 
 const authRouter = Router();
 //signup
 authRouter.post(
   '/register',
+  validateBody(authRegisterSchema),
 
   ctrlWrapper(registerController),
+);
+
+authRouter.post(
+  '/login',
+  validateBody(authLoginSchema),
+  ctrlWrapper(loginController),
 );
 
 export default authRouter;
